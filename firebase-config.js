e725652bfbfdf0f8ba1fabdb5992f21c62215cf6
tingtechit <tingtechit@gmail.com>
2026-02-20 20:53:45 +0530

    Expose voice chat controls on gameplay page

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCmRSLgpM93wi-kp-NZnaryDzmWjfU3XeE",
+  authDomain: "cards-f6618.firebaseapp.com",
+  databaseURL: "https://cards-f6618-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "cards-f6618",
+  storageBucket: "cards-f6618.firebasestorage.app",
+  messagingSenderId: "105494867189",
+  appId: "1:105494867189:web:8ea23072f75446e45529bc",
+  measurementId: "G-F0LFL93Z4X"
+};
